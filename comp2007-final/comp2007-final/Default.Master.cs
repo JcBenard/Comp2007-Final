431f52dab186e4930cbbeb4f12c0c115c96f35ee
JcBenard <niteburn@hotmail.com>
2015-07-27 11:34:53 -0400

    Created base files from template

---
+++ comp2007-final/comp2007-final/Default.Master.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Owin.Security;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace comp2007_final
+{
+    public partial class Default : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                PrivateNav.Visible = false;
+                PublicNav.Visible = true;
+            }
+            else
+            {
+                PrivateNav.Visible = true;
+                PublicNav.Visible = false;
+            }
+        }
+
+        protected void SignOut(object sender, EventArgs e)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            authenticationManager.SignOut();
+            Response.Redirect("~/Default.aspx");
+        }
+    }
+}
